5120126ddd089c6502cc61a0c229e51c5dd02b54
MrSOHIT <142032180+MrSOHIT@users.noreply.github.com>
2025-02-11 21:14:18 +0545

    Update test.py
    api key changes

--- myenv/test.py
+++ myenv/test.py
@@ -1,6 +1,6 @@
 import requests
 
-newsapi = "2e14f25cf0ea4eb6bb425c8ebc4eab22"
+newsapi = "your_api_key"
 response = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
 
 print("Status Code:", response.status_code)
